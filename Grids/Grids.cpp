1d929db0c940d763134554fed61ce03462cba67b
GinDoyr <180967757+GDVevo@users.noreply.github.com>
2024-10-29 20:50:46 +0300

    add task 3
    dog

---
+++ Grids/Grids.cpp
@@ -0,0 +1,54 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    sf::RenderWindow window(sf::VideoMode(500, 500), L"Матрица: Перезагрузка");
+    
+    const int cellSize = 50;
+    const int gridSize = 10;
+    
+    sf::RectangleShape cells[gridSize][gridSize];
+    
+    for (int y = 0; y < gridSize; ++y)
+    {
+        for (int x = 0; x < gridSize; ++x)
+        {
+            cells[x][y].setSize(sf::Vector2f(cellSize - 2, cellSize - 2));
+            cells[x][y].setPosition(y * cellSize, x * cellSize);
+            cells[x][y].setOutlineColor(sf::Color::Black);
+            cells[x][y].setOutlineThickness(1);
+
+            //Проверка по условию задания
+            if (x-y<=4 and x>=4 and x+y <= 13)
+            {
+                cells[x][y].setFillColor(sf::Color::Green);
+            }
+            else
+            {
+                cells[x][y].setFillColor(sf::Color::White);
+            }
+        }
+    }
+    while (window.isOpen())
+    {  
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::White);
+        for (int y = 0;y < gridSize; ++y)
+        {
+            for (int x = 0; x < gridSize; ++x)
+            {
+                 window.draw(cells[x][y]);
+            }
+        }
+        window.display();
+    }
+    return 0;
+}
